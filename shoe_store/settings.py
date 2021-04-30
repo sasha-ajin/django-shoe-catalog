94c1138b33737f49c222bf47193029c3ed7d3117
Sasha Ajin <77781886+sasha-ajin@users.noreply.github.com>
2021-04-30 19:16:28 +0300

    Update settings.py

--- shoe_store/settings.py
+++ shoe_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_0+oaj--3v*rgbl@r_rq=p9hh)u^-ufzi#*9l7rukl3pou*_v*'
+SECRET_KEY = 'Hi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
